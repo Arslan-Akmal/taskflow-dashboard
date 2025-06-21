d2444ec36b2a4105dfe108c85b385688fae732f6
Arslan01 <raja.arslanakmal786@gmail.com>
2025-06-21 20:55:19 +0100

    Refactor: use .env file for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,23 @@
 // Import Firebase core + services
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
+// import { getAnalytics } from 'firebase/analytics'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAhK-WXCF282ZXjDT5Fmo2Hvfy5JYItwxM',
-  authDomain: 'taskflow-83dd3.firebaseapp.com',
-  projectId: 'taskflow-83dd3',
-  storageBucket: 'taskflow-83dd3.firebasestorage.app',
-  messagingSenderId: '522031342617',
-  appId: '1:522031342617:web:85cdfbd04e5494e8797688',
-  measurementId: 'G-SVJQQXZ93Z',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+// const analytics = getAnalytics(app)
 const auth = getAuth(app)
 const db = getFirestore(app)
 
